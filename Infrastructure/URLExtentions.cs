52b0c810c26cabc90a9bcb6ac22ae2ee88bcda39
Mckay Moulton <mckaymoulton1@gmail.com>
2022-02-23 22:55:41 -0700

    Works like a charm

--- Infrastructure/URLExtentions.cs
+++ Infrastructure/URLExtentions.cs
@@ -9,6 +9,6 @@ namespace Bookstore.Infrastructure
     public static class URLExtentions
     {
         public static string PathAndQuery(this HttpRequest request) =>
-            request.QueryString.HasValue ? $"{request.Path}{request.QueryString}" : request.PathAndQuery().ToString();
+            request.QueryString.HasValue ? $"{request.Path}{request.QueryString}" : request.Path.ToString();
     }
 }
