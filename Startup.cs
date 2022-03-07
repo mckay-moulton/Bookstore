0524762c674456b0d87cb77b20fa41f7f771ea1a
Mckay Moulton <mckaymoulton1@gmail.com>
2022-03-07 11:35:05 -0700

    Video 7 of P1 complete

--- Startup.cs
+++ Startup.cs
@@ -44,6 +44,8 @@ namespace Bookstore
             //did this for Bookstore week 3 session video
             services.AddScoped<Basket>(x => SessionBasket.GetBasket(x));
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            services.AddServerSideBlazor();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -90,6 +92,9 @@ namespace Bookstore
                 endpoints.MapDefaultControllerRoute();
 
                 endpoints.MapRazorPages();
+
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/admin/{*catchall}", "/Admin/Index");
             });
         }
     }
